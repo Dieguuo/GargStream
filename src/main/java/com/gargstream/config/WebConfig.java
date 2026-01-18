2880c6c3c22b95dd7e8e54bcfb13e871b7c3b0fa
Dieguuo <diegopzzv@gmail.com>
2026-01-18 10:29:41 +0100

    Commit16 - No guardar avatares.

--- src/main/java/com/gargstream/config/WebConfig.java
+++ src/main/java/com/gargstream/config/WebConfig.java
@@ -1,13 +1,21 @@
 package com.gargstream.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    //la ruta que he puesto en application properties
+    @Value("${gargstream.upload.path}")
+    private String uploadPath;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -16,6 +24,15 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/api/archivos/**")
                 .addResourceLocations("file:mediafiles/")
                 .setCachePeriod(0);
+
+
+        // para los avatares no se guarden en img
+        // convierte la ruta relativa uploads/ a absoluta para evitar errores
+        Path rutaUploads = Paths.get(uploadPath);
+        String rutaUri = rutaUploads.toAbsolutePath().toUri().toString();
+
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations(rutaUri);
     }
 
     // decirle al server que es vtt para los subtitulos
@@ -23,4 +40,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
         configurer.mediaType("vtt", MediaType.parseMediaType("text/vtt"));
     }
+
+
+
 }
